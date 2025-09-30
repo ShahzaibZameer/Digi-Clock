15e78267bf2f69be32a42f0192d64f556b115e4e
Daniyal Saqib <daniyalsaqib04@gmail.com>
2025-09-30 13:18:07 +0500

    javascript file to be checked

---
+++ script.js
@@ -0,0 +1,37 @@
+function updateClock() {
+  const now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  // Add leading zeros
+  if (hours < 10) {
+  hours = "0" + hours;
+} else {
+  hours = hours;
+}
+
+if (minutes < 10) {
+  minutes = "0" + minutes;
+} else {
+  minutes = minutes;
+}
+
+if (seconds < 10) {
+  seconds = "0" + seconds;
+} else {
+  seconds = seconds;
+}
+
+  const timeString = hours + ":" + minutes + ":" + seconds;
+
+  document.getElementById("clock").textContent = timeString;
+  console.log(timeString);
+}
+
+// Update every second
+setInterval(updateClock, 1000);
+
+// Run once at start
+updateClock();
